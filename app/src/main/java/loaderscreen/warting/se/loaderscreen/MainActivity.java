7fbcfb4cddaf5e75ed5a40f6e2d3625c3fb9486f
Stefan <stefanw@spotify.com>
2018-11-30 08:36:50 +0100

    Update libs + removed unnecessary code for the sake of this demo

--- app/src/main/java/loaderscreen/warting/se/loaderscreen/MainActivity.java
+++ app/src/main/java/loaderscreen/warting/se/loaderscreen/MainActivity.java
@@ -1,71 +1,27 @@
 package loaderscreen.warting.se.loaderscreen;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.NotificationCompat;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import loaderscreen.warting.se.loaderscreen.databinding.ActivityMainBinding;
-
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setTitle("Main");
-        setUpView(binding.appBarMain.toolbar, binding.navView);
-        overridePendingTransition(0, 0);
 
 
-        binding.appBarMain.contentMain.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        setContentView(R.layout.activity_main);
 
-
-                Intent intent = new Intent(MainActivity.this, ToolsActivity.class);
-                PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                NotificationCompat.Builder b = new NotificationCompat.Builder(MainActivity.this)
-                        .setDefaults(Notification.DEFAULT_ALL)
-                        .setWhen(System.currentTimeMillis())
-                        .setTicker("Loader Screen")
-                        .setSmallIcon(R.drawable.ic_menu_camera)
-                        .setAutoCancel(false)
-                        .setContentTitle("Loaderscreen deeplink")
-                        .setContentText("Test deep link behavior")
-                        .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
-                        .setContentIntent(contentIntent)
-                        .setContentInfo("Info");
-
-
-                NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManager.notify(1, b.build());
-            }
-        });
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 }
